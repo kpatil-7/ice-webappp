1ba2ba8bc51c11812a44dd1725c13f26ea2608da
kayenat patil <kpatil7@hawk.iit.edu>
2023-10-24 15:58:31 -0400

    3000 addresses

--- src/Overview.js
+++ src/Overview.js
@@ -8,7 +8,7 @@ function Overview() {
   const [selectedStatus, setSelectedStatus] = useState('All');
 
   useEffect(() => {
-    fetch('http://localhost:5000/getAllTests')
+    fetch('http://34.150.173.125:3000/getAllTests')
       .then((response) => response.json())
       .then((data) => {
         if (data.testsData) {
