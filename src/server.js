025a05dc7a258b85bfe17f75cad2157eb99e2cb4
kayenat patil <kpatil7@hawk.iit.edu>
2023-10-24 14:34:14 -0400

    port updated

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const mysql = require('mysql2');
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
