5f279ba019e3fe684c2dd6742f30e8d90cd52500
kayenat patil <kpatil7@hawk.iit.edu>
2023-10-24 15:54:33 -0400

    changed welcome and login fetches

--- src/Login.js
+++ src/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
   }, []);
 
   const handleLogin = async () => {
-    const response = await fetch('http://localhost:5000/login', {
+    const response = await fetch('http://34.150.173.125:5000/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
